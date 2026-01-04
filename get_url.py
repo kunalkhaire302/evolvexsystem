e1d80ea3012bdd6650a3afc61634232f0bfd8ecb
Kunal Khaire <148051608+kunalkhaire302@users.noreply.github.com>
2026-01-04 23:30:05 +0530

    d

--- get_url.py
+++
@@ -1,7 +0,0 @@
-import requests
-import json
-try:
-    resp = requests.get('http://localhost:8088/', headers={'Expo-Platform': 'android', 'Accept': 'application/expo+json,application/json'})
-    print(json.dumps(resp.json(), indent=2))
-except Exception as e:
-    print(e)
